ddf36e2bcd9748e3fa6de1239354af97e0bb47dc
Angel <angel.villrey@gmail.com>
2026-01-31 22:39:29 -0600

    ESP32 SENSOR

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
+data = {"temp": 0, "hum": 0}
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", data=data)
+
+@app.route("/datos", methods=["POST"])
+def recibir():
+    global data
+    data = request.get_json()
+    return jsonify({"status":"ok"})
