f1822a08f02aacec944bd55e219e8324fcb52876
Angel <angel.villrey@gmail.com>
2026-01-31 23:13:18 -0600

    Actualizacion datos en tiempo real

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
-data = {"temp": 0, "hum": 0}
+
+ultimo = {"temp": 0, "hum": 0}
 
 @app.route("/")
 def home():
-    return render_template("index.html", data=data)
+    return render_template("index.html")
 
 @app.route("/datos", methods=["POST"])
 def recibir():
-    global data
-    data = request.get_json()
-    return jsonify({"status":"ok"})
+    global ultimo
+    ultimo = request.get_json()
+    return "ok"
+
+@app.route("/ver")
+def ver():
+    return jsonify(ultimo)
